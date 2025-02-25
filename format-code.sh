d8a0b0fc86e2e7959b176ea34b56a37e3864317f
Suvindas <suvindas1235@gmail.com>
2025-02-26 00:14:52 +0100

    Update format-code.sh

--- format-code.sh
+++ format-code.sh
@@ -5,13 +5,23 @@ JARFILE=google-java-format-$VERSION-all-deps.jar
 
 mkdir -p .cache
 cd .cache
-if [ ! -f $JARFILE ]
-then
+
+if [ ! -f "$JARFILE" ]; then
     curl -LJO "https://github.com/google/google-java-format/releases/download/v$VERSION/$JARFILE"
-    chmod 755 $JARFILE
+    chmod 755 "$JARFILE"
 fi
+
 cd ..
 
-changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
-echo $changed_java_files
-java -jar .cache/$JARFILE --aosp --replace $changed_java_files
+changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*\.java$" )
+
+if [[ -n "$changed_java_files" ]]; then  # Check if any files were found
+    echo "Formatting these files:"
+    echo "$changed_java_files"
+
+    # Use xargs to handle potential spaces in filenames
+    echo "$changed_java_files" | xargs -0 -n 1 java -jar .cache/"$JARFILE" --aosp --replace
+
+else
+    echo "No staged Java files found to format."
+fi
